5b0a6473267a309068ded17e615c18ea40a472f9
singoreS <singore.sissoko@gmail.com>
2023-09-21 17:47:14 +0200

    first commit

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,16 +1,27 @@
-import Contact1 from "../../component/Contactsection2.jsx/Contact1";
-import Contact2 from "../../component/Contactsection2.jsx/Contact2";
+
 import Footer from "../../component/Footer/Footer";
 import Nav from "../../component/Nav/Nav";
-import "./Contact.css";
+import "./Contact.css"
 
 const Contact = () => {
   return (
     <>
+      <div>
       <Nav />
-      <Contact1 />
-      <Contact2 />
-      <Footer />
+      <span id="smiley1" />
+  <span />
+    <h1>Contact</h1>
+    <p>
+      Avez-vous des questions, des suggestions ou souhaitez-vous en savoir plus
+      sur notre initiative Intergen ? <br />
+      Nous sommes là pour vous aider ! Remplissez le formulaire ci-dessous et
+      nous vous contacterons dans les plus brefs délais par mail.
+    </p>
+    <div>
+      <img src="public/img/8690678_3969584.svg" alt="img-contact" />
+    </div>
+        <Footer />
+        </div>
     </>
 
   );
